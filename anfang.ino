6a35e8584ec846f4cd6f092c892f14e3e81be83e
Khaled Elorbany <34633428+KhaledElOrbany@users.noreply.github.com>
2024-06-30 08:10:32 +0300

    Update anfang.ino

--- anfang.ino
+++ anfang.ino
@@ -1,24 +1,8 @@
-const short RED = 5;
-const short GREEN = 6;
-const short BLUE = 7;
 
 void setup() {
-  pinMode(RED, OUTPUT);
-  pinMode(GREEN, OUTPUT);
-  pinMode(BLUE, OUTPUT);
+  
 }
 
 void loop() {
-  digitalWrite(RED, LOW);
-  digitalWrite(GREEN, HIGH);
-  digitalWrite(BLUE, HIGH);
-  delay(500);
-  digitalWrite(RED, HIGH);
-  digitalWrite(GREEN, LOW);
-  digitalWrite(BLUE, HIGH);
-  delay(500);
-  digitalWrite(RED, HIGH);
-  digitalWrite(GREEN, HIGH);
-  digitalWrite(BLUE, LOW);
-  delay(500);
+  
 }
